6e07d262d3ac1b8db421c49ab990a96caf85d576
Katie Lund <kathleen.lund8@gmail.com>
2020-06-30 19:29:25 +0000

    Remove unnecessary imports

--- capstone/src/main/java/com/google/step/servlets/RestaurantServlet.java
+++ capstone/src/main/java/com/google/step/servlets/RestaurantServlet.java
@@ -14,22 +14,17 @@
 
 package com.google.step.servlets;
 
-import com.google.appengine.api.datastore.Cursor;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.GeoPt;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.datastore.QueryResultList;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.step.data.Restaurant;
 import java.io.IOException;
-import java.lang.Integer;
-import java.lang.Number;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
