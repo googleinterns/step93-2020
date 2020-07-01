9ef8558063c93fbfb696a9818ac39b8f3a95e4eb
Martin <mheberling@google.com>
2020-07-01 22:30:47 +0000

    Adressed changes.

--- capstone/src/main/java/com/google/step/servlets/LoginServlet.java
+++ capstone/src/main/java/com/google/step/servlets/LoginServlet.java
@@ -7,16 +7,14 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import java.util.Map;
-import java.util.HashMap;
-import com.google.gson.Gson;
 
 /**
  * Servlet that will handle login information.
@@ -24,82 +22,82 @@ import com.google.gson.Gson;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json;");
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json;");
+    UserService userService = UserServiceFactory.getUserService();
+    Map<String, Object> login = new HashMap<>();
 
-        UserService userService = UserServiceFactory.getUserService();
-        Map<String, Object> login = new HashMap<>();
+    if (userService.isUserLoggedIn()) {
+      String useremail = userService.getCurrentUser().getEmail();
+      String type = checkIfRestaurantOrUser(useremail);
 
-        if (userService.isUserLoggedIn()) {
-            String useremail = userService.getCurrentUser().getEmail();
-            String type = checkIfRestaurantOrUser(useremail);
+      // If user is loggedin but not in our system.
+      if (type == null) {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        return;
+      }
 
-            //If user is loggedin but not in our system.
-            if (type == null) {
-                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                return;
-            }
+      String urltoRedirectToAfterUserLogsOut = "/";
+      String logoutUrl = userService.createLogoutURL(urltoRedirectToAfterUserLogsOut);
 
-            String urltoRedirectToAfterUserLogsOut = "/";
-            String logoutUrl = userService.createLogoutURL(urltoRedirectToAfterUserLogsOut);
+      login.put("Loggedin", true);
+      login.put("Type", type);
+      login.put("Email", useremail);
+      login.put("logOutUrl", logoutUrl);
+    } else {
+      String urltoRedirectToAfterUserLogIn = "/";
+      String urltoRedirectAfterUserSignUp = "/user-signup.html";
+      String urltoRedirectAfterRestaurantSignUp = "/restaurant-signup.html";
 
-            login.put("Loggedin", true);
-            login.put("Type", type);
-            login.put("Email", useremail);
-            login.put("logOutUrl", logoutUrl);
-        } else {
+      String loginUrl = userService.createLoginURL(urltoRedirectToAfterUserLogIn);
+      String userSignUpUrl = userService.createLoginURL(urltoRedirectAfterUserSignUp);
+      String restaurantSignUpUrl = userService.createLoginURL(urltoRedirectAfterRestaurantSignUp);
 
-            String urltoRedirectToAfterUserLogIn = "/";
-            String urltoRedirectAfterUserSignUp = "/user-signup.html";
-            String urltoRedirectAfterRestaurantSignUp = "/restaurant-signup.html";
-
-            String loginUrl = userService.createLoginURL(urltoRedirectToAfterUserLogIn);
-            String userSignUpUrl = userService.createLoginURL(urltoRedirectAfterUserSignUp);
-            String restaurantSignUpUrl = userService.createLoginURL(urltoRedirectAfterRestaurantSignUp);
-
-            login.put("Loggedin", false);
-            login.put("loginUrl", loginUrl);
-            login.put("userSignUpUrl", userSignUpUrl);
-            login.put("restaurantSignUpUrl", restaurantSignUpUrl);
-        }
-
-        String json = convertToJsonUsingGson(login);
-        response.getWriter().println(json);
+      login.put("Loggedin", false);
+      login.put("loginUrl", loginUrl);
+      login.put("userSignUpUrl", userSignUpUrl);
+      login.put("restaurantSignUpUrl", restaurantSignUpUrl);
     }
 
-    // TODO: Change to have object return and return User or Restaurant Object
-    /**
-     * Checks if the current email is from a user or a restaurant.
-     * @param email of the current user.
-     * @return String containing the words User or Restaurant.
-     */
-    private static String checkIfRestaurantOrUser(String email) {
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Query query = new Query("User").setFilter(new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, email));
-        PreparedQuery results = datastore.prepare(query);
-        Entity entity = results.asSingleEntity();
-        if (entity != null) {
-            return "User";
-        } else {
-            query = new Query("RestaurantUser").setFilter(new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, email));
-            if (entity != null) {
-                return "Restaurant";
-            } else {
-                return null;
-            }
-        }
-    }
+    String json = convertToJsonUsingGson(login);
+    response.getWriter().println(json);
+  }
 
-    /**
-     * Converts to Json using Gson.
-     * @param login Map to convert to Gson.
-     * @return String in JSON format.
-     */
-    private static String convertToJsonUsingGson(Map login) {
-        Gson gson = new Gson();
-        String json = gson.toJson(login);
-        return json;
+  // TODO: Change to have object return and return User or Restaurant Object
+  /**
+   * Checks if the current email is from a user or a restaurant.
+   * @param email of the current user.
+   * @return String containing the words User or Restaurant.
+   */
+  private static String checkIfRestaurantOrUser(String email) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query("User").setFilter(
+        new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, email));
+    PreparedQuery results = datastore.prepare(query);
+    Entity entity = results.asSingleEntity();
+    if (entity != null) {
+      return "User";
+    } else {
+      query = new Query("RestaurantUser")
+                  .setFilter(new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, email));
+      if (entity != null) {
+        return "Restaurant";
+      } else {
+        return null;
+      }
     }
+  }
+
+  /**
+   * Converts to Json using Gson.
+   * @param login Map to convert to Gson.
+   * @return String in JSON format.
+   */
+  private static String convertToJsonUsingGson(Map login) {
+    Gson gson = new Gson();
+    String json = gson.toJson(login);
+    return json;
+  }
 }
--- capstone/src/main/java/com/google/step/servlets/UserSignUpServlet.java
+++ capstone/src/main/java/com/google/step/servlets/UserSignUpServlet.java
@@ -7,8 +7,6 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-
-import java.io.IOException;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,37 +18,37 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/userSignUp")
 public class UserSignUpServlet extends HttpServlet {
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        UserService userService = UserServiceFactory.getUserService();
-        if(!userService.isUserLoggedIn()) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return;
-        }
-
-        String userName = request.getParameter("userName");
-        String id = userService.getCurrentUser().getUserId();
-        String email = userService.getCurrentUser().getEmail();
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-
-        // Check if user email already exists.
-        Query query = new Query("User").setFilter(new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, email));
-        PreparedQuery results = datastore.prepare(query);
-        Entity entity = results.asSingleEntity();
-        if (entity != null) {
-            response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
-            return;
-        }
-
-        Entity userEntity = new Entity("UserInfo", id);
-        userEntity.setProperty("id", id);
-        userEntity.setProperty("email", email);
-        userEntity.setProperty("userName", userName);
-
-        datastore.put(userEntity);
-
-        response.sendRedirect("/index.html");
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      return;
     }
+
+    String userName = request.getParameter("userName");
+    String id = userService.getCurrentUser().getUserId();
+    String email = userService.getCurrentUser().getEmail();
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    // Check if user email already exists.
+    Query query = new Query("User").setFilter(
+        new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, email));
+    PreparedQuery results = datastore.prepare(query);
+    Entity entity = results.asSingleEntity();
+    if (entity != null) {
+      response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+      return;
+    }
+
+    Entity userEntity = new Entity("UserInfo", id);
+    userEntity.setProperty("id", id);
+    userEntity.setProperty("email", email);
+    userEntity.setProperty("userName", userName);
+
+    datastore.put(userEntity);
+
+    response.sendRedirect("/index.html");
+  }
 }
