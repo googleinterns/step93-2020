2d0f9e4b0c3a5126145381caf2a4ef47413b00b4
Katie Lund <kathleen.lund8@gmail.com>
2020-07-08 16:36:32 +0000

    Add unit testing for new restaurant class method.

--- capstone/src/main/java/com/google/step/data/Restaurant.java
+++ capstone/src/main/java/com/google/step/data/Restaurant.java
@@ -14,11 +14,10 @@
 
 package com.google.step.data;
 
+import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.GeoPt;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.google.appengine.api.datastore.Entity;
 
 /** A restaurant to be displayed. */
 public final class Restaurant {
@@ -43,11 +42,11 @@ public final class Restaurant {
     this.status = status;
   }
 
-/**
- * Factory method to return a Restaurant based on an Entity
- * @param entity
- * @return Restaurant, or null if the entity is the wrong kind
- */
+  /**
+   * Factory method to return a Restaurant based on an Entity
+   * @param entity
+   * @return Restaurant, or null if the entity is the wrong kind
+   */
   public static Restaurant fromEntity(Entity entity) {
     if (!entity.getKind().equals("RestaurantInfo")) {
       return null;
@@ -62,7 +61,58 @@ public final class Restaurant {
     String status = (String) entity.getProperty("status");
 
     // Return Restaurant object holding all info
-    return new Restaurant(
-        restaurantKey, name, location, story, cuisine, phone, website, status);
+    return new Restaurant(restaurantKey, name, location, story, cuisine, phone, website, status);
+  }
+
+  /**
+   * equals method for use in testing.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Restaurant other = (Restaurant) obj;
+    if (cuisine == null) {
+      if (other.cuisine != null)
+        return false;
+    } else if (!cuisine.equals(other.cuisine))
+      return false;
+    if (location == null) {
+      if (other.location != null)
+        return false;
+    } else if (!location.equals(other.location))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (phone == null) {
+      if (other.phone != null)
+        return false;
+    } else if (!phone.equals(other.phone))
+      return false;
+    if (restaurantKey != other.restaurantKey)
+      return false;
+    if (status == null) {
+      if (other.status != null)
+        return false;
+    } else if (!status.equals(other.status))
+      return false;
+    if (story == null) {
+      if (other.story != null)
+        return false;
+    } else if (!story.equals(other.story))
+      return false;
+    if (website == null) {
+      if (other.website != null)
+        return false;
+    } else if (!website.equals(other.website))
+      return false;
+    return true;
   }
 }
--- capstone/src/main/java/com/google/step/data/SeedData.java
+++ capstone/src/main/java/com/google/step/data/SeedData.java
@@ -27,26 +27,30 @@ import java.util.List;
 public class SeedData {
   private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-/**
- * Seeds desired data. Change any method calls or add new data types to use the script
- * for different types of testing when needed.
- */
+  /**
+   * Seeds desired data. Change any method calls or add new data types to use the script
+   * for different types of testing when needed.
+   */
   public static void main(String[] args) {
-    seedRestaurantInfo(0, "McDonald's", new GeoPt((float) 42.297522, (float) -87.956039), 
-        "We're a global business with the best french fries around.", "American,Fast food", "(847)362-3040",
-        "https://www.mcdonalds.com/us/en-us/location/il/libertyville/1330-n-milwaukee-ave/119.html?cid=RF:YXT:GMB::Clicks", "GOOD", 3.5);
+    seedRestaurantInfo(0, "McDonald's", new GeoPt((float) 42.297522, (float) -87.956039),
+        "We're a global business with the best french fries around.", "American,Fast food",
+        "(847)362-3040",
+        "https://www.mcdonalds.com/us/en-us/location/il/libertyville/1330-n-milwaukee-ave/119.html?cid=RF:YXT:GMB::Clicks",
+        "GOOD", 3.5);
     seedRestaurantInfo(1, "Casa Bonita", new GeoPt((float) 42.289267, (float) -87.954920),
-        "Classic Mexican eats & over 150 tequilas in a colorful, fiesta setting with a heated patio."," Mexican",
-        "(847)362-4400", "https://www.casabonitalibertyville.com","OKAY", 2.5);
-    seedRestaurantInfo(2, "Lauretta's", new GeoPt((float) 42.2532875, (float) -88.0003111), 
+        "Classic Mexican eats & over 150 tequilas in a colorful, fiesta setting with a heated patio.",
+        " Mexican", "(847)362-4400", "https://www.casabonitalibertyville.com", "OKAY", 2.5);
+    seedRestaurantInfo(2, "Lauretta's", new GeoPt((float) 42.2532875, (float) -88.0003111),
         "Quaint cafe & bakeshop whipping up Italian favorites, cakes & cookies in a snug space with a patio.",
-        "Italian,Cafe,Bakery","(847)566-0883","https://www.laurettasbakeshop.info","STRUGGLING", 2.0);
-    seedRestaurantInfo(3, "Thai Noodles Cafe", new GeoPt((float) 42.2803482, (float) -87.9528155), 
+        "Italian,Cafe,Bakery", "(847)566-0883", "https://www.laurettasbakeshop.info", "STRUGGLING",
+        2.0);
+    seedRestaurantInfo(3, "Thai Noodles Cafe", new GeoPt((float) 42.2803482, (float) -87.9528155),
         "Relaxed Thai eatery serving traditional dishes in a quaint setting inside a converted house.",
-        "Thai","(847)362-3494","https://www.easyordering.com/local/thainoodlecafe","OKAY",2.3);
-    seedRestaurantInfo(4, "Wildfire", new GeoPt((float) 42.1784405, (float) -87.9284299), 
-        "Swanky American chain serving steak, chops & seafood, plus burgers, sides & cocktails.","Steakhouse,American",
-        "(847)279-7900","https://www.wildfirerestaurant.com","STRUGGLING", 1.1);
+        "Thai", "(847)362-3494", "https://www.easyordering.com/local/thainoodlecafe", "OKAY", 2.3);
+    seedRestaurantInfo(4, "Wildfire", new GeoPt((float) 42.1784405, (float) -87.9284299),
+        "Swanky American chain serving steak, chops & seafood, plus burgers, sides & cocktails.",
+        "Steakhouse,American", "(847)279-7900", "https://www.wildfirerestaurant.com", "STRUGGLING",
+        1.1);
 
     seedRestaurantUser(0, "mcdonalds@gmail.com");
     seedRestaurantUser(1, "casabonita@gmail.com");
@@ -55,28 +59,28 @@ public class SeedData {
     seedRestaurantUser(4, "wildfire@gmail.com");
   }
 
-/**
- * Creates a RestaurantInfo Entity and puts it in Datastore.
- */
-  private static void seedRestaurantInfo(long restaurantKey, String name, GeoPt location, String story,
-      String cuisines, String phone, String website, String status, double score) {
-        Entity restaurantInfo = new Entity("RestaurantInfo");
-        restaurantInfo.setProperty("restaurantKey", restaurantKey);
-        restaurantInfo.setProperty("name", name);
-        restaurantInfo.setProperty("location", location);
-        restaurantInfo.setProperty("story", story);
-        List<String> cuisineList = Arrays.asList(cuisines.split(","));
-        restaurantInfo.setProperty("cuisine", cuisineList);
-        restaurantInfo.setProperty("phone", "(847)362-3040");
-        restaurantInfo.setProperty("website", website);
-        restaurantInfo.setProperty("score", score);
-        restaurantInfo.setProperty("status", status);
-        datastore.put(restaurantInfo);
+  /**
+   * Creates a RestaurantInfo Entity and puts it in Datastore.
+   */
+  private static void seedRestaurantInfo(long restaurantKey, String name, GeoPt location,
+      String story, String cuisines, String phone, String website, String status, double score) {
+    Entity restaurantInfo = new Entity("RestaurantInfo");
+    restaurantInfo.setProperty("restaurantKey", restaurantKey);
+    restaurantInfo.setProperty("name", name);
+    restaurantInfo.setProperty("location", location);
+    restaurantInfo.setProperty("story", story);
+    List<String> cuisineList = Arrays.asList(cuisines.split(","));
+    restaurantInfo.setProperty("cuisine", cuisineList);
+    restaurantInfo.setProperty("phone", "(847)362-3040");
+    restaurantInfo.setProperty("website", website);
+    restaurantInfo.setProperty("score", score);
+    restaurantInfo.setProperty("status", status);
+    datastore.put(restaurantInfo);
   }
 
-/**
- * Creates a RestaurantUser Entity and puts it in Datastore.
- */
+  /**
+   * Creates a RestaurantUser Entity and puts it in Datastore.
+   */
   private static void seedRestaurantUser(long restaurantKey, String email) {
     Entity restaurantUser = new Entity("RestaurantUser");
     restaurantUser.setProperty("restaurantKey", restaurantKey);
