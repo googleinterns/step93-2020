41a4dc55f62f244747a07ca92e57ae26afc9c029
Martin <mheberling@google.com>
2020-07-08 23:44:14 +0000

    Changed RestaurantSignUpServlet -> RestaurantServlet.

--- capstone/src/main/java/com/google/step/servlets/RestaurantServlet.java
+++ capstone/src/main/java/com/google/step/servlets/RestaurantServlet.java
@@ -6,6 +6,9 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.GeoPt;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
@@ -17,11 +20,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet that will put the restaurant information in Datastore.
- */
-@WebServlet("/restaurantSignUp")
-public class RestaurantSignUpServlet extends HttpServlet {
+/** Servlet responsible for sending/getting a restaurant to/from Datastore. */
+@WebServlet("/restaurant")
+public class RestaurantServlet extends HttpServlet {
+
+  private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+  /**
+   * Will post a new restaurant into datastore.
+   * @param request sent by the frontend.
+   * @param response to the frontend.
+   * @throws IOException
+   */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     UserService userService = UserServiceFactory.getUserService();
@@ -35,26 +45,6 @@ public class RestaurantSignUpServlet extends HttpServlet {
     String id = userService.getCurrentUser().getUserId();
     String email = userService.getCurrentUser().getEmail();
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-
-    // TODO: add a check if the email already exists as a user
-    // If the restaurant email already exists in datastore don't add it.
-    Query query =
-        new Query("RestaurantUser")
-            .setFilter(new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, email));
-    PreparedQuery results = datastore.prepare(query);
-    Entity resultEntity = results.asSingleEntity();
-    if (resultEntity != null) {
-      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-      return;
-    }
-
-    // Add current user to RestaurantUser entity.
-    Entity restaurantUser = new Entity("RestaurantUser", id);
-    restaurantUser.setProperty("restaurantKey", id);
-    restaurantUser.setProperty("email", email);
-    datastore.put(restaurantUser);
-
     String name = request.getParameter("name");
 
     // Split the cuisine into a list
@@ -65,7 +55,7 @@ public class RestaurantSignUpServlet extends HttpServlet {
 
     // Make sure its only numbers
     String phone = request.getParameter("phone");
-    phone = phone.replaceAll("^[\\p{IsDigit}]", "");
+    phone = changeNonNumericCharacters(phone);
 
     // TODO: Make sure its a link.
     String website = request.getParameter("website");
@@ -76,6 +66,7 @@ public class RestaurantSignUpServlet extends HttpServlet {
     Entity restaurantInfo = new Entity("RestaurantInfo", id);
     restaurantInfo.setProperty("restaurantKey", id);
     restaurantInfo.setProperty("name", name);
+    restaurantInfo.setProperty("email", email);
     restaurantInfo.setProperty("location", geoPoint);
     restaurantInfo.setProperty("story", story);
     restaurantInfo.setProperty("cuisine", cuisineList);
@@ -89,5 +80,9 @@ public class RestaurantSignUpServlet extends HttpServlet {
     datastore.put(restaurantInfo);
 
     response.sendRedirect("/index.html");
+  }
+
+  private String changeNonNumericCharacters(String num) {
+    return num.replaceAll("[^0-9]", "");
   }
 }
