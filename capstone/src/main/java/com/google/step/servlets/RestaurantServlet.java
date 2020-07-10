9e84a4c9281f46b5013a4c8a0f266aa4625b80a0
Martin Heberling <mheberling@google.com>
2020-07-10 16:41:03 +0000

    Added servlets to handle login and sign up for users and restaurants. (#2)
    * Added servlets to handle login and sign up for users and restaurants.
    
    * Refactored and changed folders.
    
    * Fixed errors.
    
    * Update lint.yml
    
    * pom.xml minor changes.
    
    * Adressed changes.
    
    * Addressed Josh comments.
    
    * SRP changes on LoginServlet class.
    
    * Addressed further Josh suggested changes.
    
    * Rollback on static issue.
    
    * Deleted potfolio.iml from the repository and minor servlet changes.
    
    * Changed RestaurantSignUpServlet -> RestaurantServlet.
    
    * Addressed changes and user-restaurant dynamic changed.
    
    * Added comments.
    
    * pom.xml errors, that would prevent compiling in Github actions changed.
    
    * Deleted duplicated class.
    
    * Addressed requested changes of style and comments.

--- capstone/src/main/java/com/google/step/servlets/RestaurantServlet.java
+++ capstone/src/main/java/com/google/step/servlets/RestaurantServlet.java
@@ -23,10 +23,15 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.google.step.data.Restaurant;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.google.step.data.Restaurant;
+
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,7 +41,78 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet responsible for sending/getting a restaurant to/from Datastore. */
 @WebServlet("/restaurant")
 public class RestaurantServlet extends HttpServlet {
+
+
   private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+  /**
+   * Will put the information gathered from the signup page and add the
+   * information into their respective properties for .
+   * @param request sent by the frontend. Requires the user to be logged in
+   *                and have the following parameters in the request body:
+   *                "name": name of the restaurant,
+   *                "cuisine": list of cuisines of the restaurant as CSV,
+   *                "story": story of the restaurant,
+   *                "phone": cellphone of the restaurant,
+   *                "website": link to their website,
+   * @param response will redirect to the home page of the website. If the
+   *                 user is not logged in when requesting the POST, the
+   *                 response will have a SC_FORBIDDEN status.
+   * @throws IOException
+   */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+
+    // Only available if the user is logged in.
+    if (!userService.isUserLoggedIn()) {
+      response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      return;
+    }
+
+    String id = userService.getCurrentUser().getUserId();
+    String email = userService.getCurrentUser().getEmail();
+
+    String name = request.getParameter("name");
+
+    // Split the cuisine into a list
+    String cuisineString = request.getParameter("cuisine");
+    List<String> cuisineList = Arrays.asList(cuisineString.split(","));
+
+    String story = request.getParameter("story");
+
+    // Make sure its only numbers
+    String phone = request.getParameter("phone");
+    phone = changeNonNumericCharacters(phone);
+
+    // TODO: Make sure its a link.
+    String website = request.getParameter("website");
+
+    // Hard coded while we don't have Maps API functionality
+    GeoPt geoPoint = new GeoPt((float) 42.23422, (float) -87.234987);
+
+    Entity restaurantInfo = new Entity("RestaurantInfo", id);
+    restaurantInfo.setProperty("restaurantKey", id);
+    restaurantInfo.setProperty("name", name);
+    restaurantInfo.setProperty("email", email);
+    restaurantInfo.setProperty("location", geoPoint);
+    restaurantInfo.setProperty("story", story);
+    restaurantInfo.setProperty("cuisine", cuisineList);
+    restaurantInfo.setProperty("phone", phone);
+    restaurantInfo.setProperty("website", website);
+
+    // Both of the following values are hardcoded while we implement the properties.
+    restaurantInfo.setProperty("score", 2.5);
+    restaurantInfo.setProperty("status", "OKAY");
+
+    datastore.put(restaurantInfo);
+
+    response.sendRedirect("/index.html");
+  }
+
+  private String changeNonNumericCharacters(String num) {
+    return num.replaceAll("[^0-9]", "");
+  }
 
   /**
    * Returns a single Restaurant based on restaurantKey.
