a7f27f6b86a3107d17a4fa7789ec1324b6e251c1
joshwash <josh8551021@gmail.com>
2020-07-10 14:13:24 +0000

    Create skeleton for ElasticsearchConnector

---
+++ capstone/src/test/java/com/google/step/search/ElasticsearchConnectorTest.java
@@ -0,0 +1,19 @@
+package com.google.step.search;
+
+public class ElasticsearchConnectorTest {
+
+  public void setUp() {
+
+  }
+
+  public void tearDown() {
+  }
+
+  public void testAddRestaurantDocumentToSearchIndex() {
+
+  }
+
+  public void testUpdateDoesNotAddNewDocumentToIndex() {
+
+  }
+}
