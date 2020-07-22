91225ad80ed9134f714849048a02fe2ae719d152
joshwash <josh8551021@gmail.com>
2020-07-22 16:21:40 +0000

    Format RestaurantServlet doGet javadoc

--- capstone/src/main/java/com/google/step/servlets/RestaurantServlet.java
+++ capstone/src/main/java/com/google/step/servlets/RestaurantServlet.java
@@ -18,16 +18,12 @@ import com.google.appengine.api.datastore.GeoPt;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.google.step.clients.RestaurantClient;
 import com.google.step.data.Restaurant;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -98,21 +94,21 @@ public class RestaurantServlet extends HttpServlet {
 
   /**
    * Returns a single Restaurant based on restaurantKey.
-   * @param response All details for the requested restaurant, in the following json format:
-    {
-      "restaurant" : {
-        "restaurantKey": long,
-        "name": String,
-        "location": GeoPt,
-        "story": String,
-        "cuisine": List<String>,
-        "phone": String,
-        "website": String,
-        "status": String
-      }
-    }
+   * @param response  All details for the requested restaurant, in the following json format:
+   *                  {
+   *                    "restaurant" : {
+   *                    "restaurantKey": long,
+   *                    "name": String,
+   *                    "location": GeoPt,
+   *                    "story": String,
+   *                    "cuisine": List<String>,
+   *                    "phone": String,
+   *                    "website": String,
+   *                    "status": String
+   *                    }
+   *                  }
    * @param request Specifies restaurant key, in the following format:
-     /restaurant?restaurantKey=int
+   *                /restaurant?restaurantKey=int
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
