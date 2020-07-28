77518f5b2c62b462105a501bf8d57b22241aeb36
Katie Lund <kathleen.lund8@gmail.com>
2020-07-28 22:39:56 +0000

    Remote API populating deployed webapp

--- capstone/src/main/java/com/google/step/clients/RestaurantClient.java
+++ capstone/src/main/java/com/google/step/clients/RestaurantClient.java
@@ -7,6 +7,7 @@ import com.google.step.data.Restaurant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 public class RestaurantClient {
   private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
