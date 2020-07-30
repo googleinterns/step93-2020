569b3124389698db9a2266bbe1614c25f9b20284
Martin <mheberling@google.com>
2020-07-30 21:48:30 +0000

    Created ScoreServlet.

---
+++ capstone/src/main/java/com/google/step/servlets/ScoreServlet.java
@@ -0,0 +1,18 @@
+package com.google.step.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet that will update the search index using the score calculations.
+ */
+public class ScoreServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+}
