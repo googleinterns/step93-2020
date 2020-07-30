93babfe2037dd44488fbe47a8102a12a3b772d04
Martin <mheberling@google.com>
2020-07-30 21:50:25 +0000

    Added webServlet endpoint.

--- capstone/src/main/java/com/google/step/servlets/ScoreServlet.java
+++ capstone/src/main/java/com/google/step/servlets/ScoreServlet.java
@@ -1,5 +1,6 @@
 package com.google.step.servlets;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +9,7 @@ import java.io.IOException;
 /**
  * Servlet that will update the search index using the score calculations.
  */
+@WebServlet("/update-score")
 public class ScoreServlet extends HttpServlet {
 
     @Override
