cd81cd4f65569209fcff440c07885e44ead1c099
Katie Lund <kathleen.lund8@gmail.com>
2020-07-30 00:09:52 +0000

    Fix extra importing

--- capstone/src/test/java/com/google/step/servlets/PageViewServletTest.java
+++ capstone/src/test/java/com/google/step/servlets/PageViewServletTest.java
@@ -1,13 +1,16 @@
 package com.google.step.servlets;
 
-import com.google.appengine.api.datastore.*;
-
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import com.google.appengine.repackaged.com.google.gson.JsonArray;
 import com.google.appengine.repackaged.com.google.gson.JsonObject;
 import com.google.appengine.repackaged.com.google.gson.JsonParser;
