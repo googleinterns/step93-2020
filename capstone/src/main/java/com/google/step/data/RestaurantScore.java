05f1778850630553fcbe33313c0fb6b8302e1fac
Martin Heberling <mheberling@google.com>
2020-07-30 20:32:53 -0400

    Added RestaurantScore class for the scoring client. (#31)
    * Added RestaurantScore class for the scoring client.
    
    * Changed restaurantName to restaurantKey.
    
    * Changed restaurantKey to long.
    
    * Changed restaurantScore to have only final values.

---
+++ capstone/src/main/java/com/google/step/data/RestaurantScore.java
@@ -0,0 +1,38 @@
+package com.google.step.data;
+
+/**
+ * Class that holds the score for a specific restaurant.
+ */
+public class RestaurantScore {
+
+    // The name should always remain the same but the score will change when calculating it.
+    private final long restaurantKey;
+    private final double score;
+
+    /**
+     * Constructor for the class.
+     * @param restaurantKey name of the restaurant.
+     * @param score for the restaurant.
+     */
+    public RestaurantScore(long restaurantKey, double score) {
+        this.restaurantKey = restaurantKey;
+        this.score = score;
+    }
+
+    /**
+     * Get the restaurant Name.
+     * @return String representing the restaurant name.
+     */
+    public long getRestaurantKey() {
+        return restaurantKey;
+    }
+
+    /**
+     * Get the score for the restaurant.
+     * @return double representing the score.
+     */
+    public double getScore() {
+        return score;
+    }
+
+}
